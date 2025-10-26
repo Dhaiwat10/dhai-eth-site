39f7b00dd34ae597fb5e86aab36f1f60c65fd840
Dhai <dhaiwatpandya@gmail.com>
2025-10-26 08:15:01 +0530

    test new router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import BlogList from "./components/BlogList";
@@ -6,7 +6,7 @@ import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -14,7 +14,7 @@ function App() {
           <Route path="blog/:id" element={<BlogPost />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
